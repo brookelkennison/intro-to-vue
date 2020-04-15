20bd33ff90be63c9f1944ea693443352cbce78cd
Brooke Kennison <brookelkennison@gmail.com>
2020-04-15 17:15:15 -0400

    finished lesson 6

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
 var app = new Vue({
-    el: '#app',
+    el: "#app",
     data: {
-        product: 'Socks',
-        image: './assets/vmSocks-green.jpg'
-    }
-})
+        product: "Socks",
+        image: "./assets/vmSocks-green.jpg",
+        inStock: true,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green",
+                variantImage: "./assets/vmSocks-green.jpg",
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue",
+                variantImage: "./assets/vmSocks-blue.jpg",
+            },
+        ],
+        cart: 0,
+    },
+    methods: {
+        addToCart: function () {
+            this.cart += 1;
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage;
+        },
+    },
+});
